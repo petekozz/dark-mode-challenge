15ddea33545c57cd0d9b157440f50b6aca36777a
petekozz <66498808+petekozz@users.noreply.github.com>
2021-05-19 20:13:35 -0700

    Update ThemeSwitcher.js

--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoon} from '@fortawesome/free-solid-svg-icons';
+import {faMoon, faSun} from '@fortawesome/free-solid-svg-icons';
+import {ThemeContext} from '../contexts';
 
 export default class ThemeSwitcher extends React.Component {
 
     render() {
         return (
-            <button className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
-            </button>
+            <ThemeContext.Consumer>
+                {({isDarkModeEnabled, toggleDarkMode}) => (
+                    <button 
+                        className="app__dark-mode-btn icon level-right" 
+                        onClick={() => toggleDarkMode()}>
+                        <FontAwesomeIcon icon={isDarkModeEnabled ? faSun : faMoon} color={isDarkModeEnabled ? '#FFA500' : '#4D5B6B'}/>
+                    </button>
+                )}
+            </ThemeContext.Consumer>
         );
     }
 
