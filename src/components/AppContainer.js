3349679340c7de812bc4bc1e8bf7d57cc6ccffdf
petekozz <66498808+petekozz@users.noreply.github.com>
2021-05-19 20:11:30 -0700

    Update AppContainer.js

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import {ThemeContext} from '../contexts';
 
 export default class AppContainer extends React.Component {
 
+    toggleDarkMode = () => {
+        this.setState(state => {
+            return {isDarkModeEnabled: !state.isDarkModeEnabled}
+        });
+    }
+
+    state = {
+        isDarkModeEnabled: false,
+        toggleDarkMode: this.toggleDarkMode,
+    }
+    
     render() {
         const {children} = this.props;
 
         return (
-            <div className="app-container">
-                {children}
+           <ThemeContext.Provider value={this.state}>
+                <div className={`app-container ${this.state.isDarkModeEnabled ? 'dark-mode' : ''}`}>
+                    {children}
+                </div>
+            </ThemeContext.Provider>
             </div>
         );
     }
